1ac93ec4594b94865366151234bb82b160a1b637
Peter_V <gordienkopeter@gmail.com>
2017-12-24 01:35:42 +0200

    add angular material design

--- src/app/common/shared.module.ts
+++ src/app/common/shared.module.ts
@@ -1,15 +1,25 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
+import {MaterialModule} from './material.module';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {HttpAuthInterceptor} from './utils/http-auth-interceptor';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    MaterialModule
   ],
   declarations: [],
   exports: [],
-  providers: []
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpAuthInterceptor,
+      multi: true
+    },
+  ]
 })
 
 export class SharedModule {
